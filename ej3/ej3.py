4e15bfc8dda92637d6d72b320ba65f42ec437e52
EzequielB@41r <ezequiel.barrios@gmail.com>
2016-12-24 17:16:55 -0300

    ..

--- ej3/ej3.py
+++ ej3/ej3.py
@@ -61,14 +61,22 @@ def print_output():
 	pass
 
 def best_polygon_for_pivot(p1):
+	print "using %s for pivot"%p1
+
 	best = 0
 	for p2 in historical_points:
 		for p3 in historical_points:
 			if p1<p2 and p2<p3:
 				t = Triangle(p1,p2,p3)
 				b = best_polygon_for_first_triangle(t)
+				print "%s is the best polygon for first triangle %s"%(b,t)
 				best = b if b > best else best
 	return best
+
+def check_internal_angle(p1,p2,p3):
+	u = Vector(p1-p2)
+	v = Vector(p3-p2)
+	return (u*v > 0)
 
 def best_polygon_for_first_triangle(t):
 	if _calculated_best_polygon_for_first_triangle[t]:
@@ -77,9 +85,18 @@ def best_polygon_for_first_triangle(t):
 		_best_polygon_for_first_triangle[t] = -1
 		_calculated_best_polygon_for_first_triangle[t] = True
 		return _best_polygon_for_first_triangle[t]
-	p2,p3 = t.p2,t.p3
-	# Recorro todos los puntos a la derecha de estos puntos, 
-	# que puedan formar triàngulos con los dos puntos de màs a la derecha de este triàngulo
+	
+	p1,p2,p3 = t.p1,t.p2,t.p3
+	best_recursive = 0
+	for recursive_p in historical_points:
+		if recursive_p > p3:
+			if check_internal_angle(p1,p2,recursive_p) and check_internal_angle(p1,p3,recursive_p):
+				recursive_t = Triangle(p2,p3,recursive_p)
+				best_recursive = max(best_polygon_for_first_triangle(recursive_t)+1, best_recursive)
+
+	_best_polygon_for_first_triangle[t] = best_recursive + 3
+	_calculated_best_polygon_for_first_triangle[t] = True
+	return _best_polygon_for_first_triangle[t]
 
 class Point:
 	""" Point class represents and manipulates x,y coords. """
@@ -104,6 +121,21 @@ class Point:
 
 	def __eq__(self, point2):
 		return self.x == point2.x and self.y == point2.y
+
+	def __sub__(self, p2):
+		return Point(self.x-p2.x, self.y-p2.y)
+	
+	def __add__(self, p2):
+		return Point(self.x+p2.x, self.y+p2.y)
+
+class Vector:
+	def __init__(self, point):
+		""" Create a new Vector """
+		self.x = point.x
+		self.y = point.y
+
+	def __mul__(self, v2):
+		(self.x*v2.y) - (v2.x*self.y)
 
 class Triangle:
 	""" Triangle class represents and manipulates 3 points figure. """
@@ -152,6 +184,7 @@ def main():
 	for pivot in historical_points:
 		actual_polygon = best_polygon_for_pivot(pivot)
 		best_polygon = actual_polygon if (actual_polygon>best_polygon) else best_polygon
+	print best_polygon
 	#print_output()
 
 if __name__ == '__main__':
